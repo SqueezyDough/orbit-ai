6bb4fce8c79d4ed10edd15269db025dc68049646
SqueezyDough <lvanbiljouw@hotmail.com>
2019-05-16 12:23:19 +0200

    Add auto-compile sass files

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const port = 3000;
 
 app
+    // .use(express.static(__dirname, + '/static'))
+    // .set('static', './static')
+    // .get('/', function (req, res) {res.render('home');})
+
     .engine('handlebars', exphbs({defaultLayout: 'main'}))
     .set('view engine', 'handlebars')
     .get('/', function (req, res) {res.render('home');})
