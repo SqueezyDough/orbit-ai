f377e88b22943ae1b51eb0977a3aaf0952937d49
SqueezyDough <lvanbiljouw@hotmail.com>
2019-05-23 22:47:10 +0200

    Add password hashing
    - add compare method

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 
 let UserSchema = new Schema({
     email: {
@@ -60,7 +62,37 @@ UserSchema.virtual('fullname')
         this.lastName = fullname.substr(fullname.indexOf(' ') + 1);
     });
 
+// validate email
 UserSchema.plugin(uniqueValidator);
 
+// Hash password
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password along with the new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the plain password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// compare user input with hashed password
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb (null, isMatch);
+    })
+}
 
 module.exports = mongoose.model('User', UserSchema);
