e46171eeeb2e9f62af296fe81407921ac9ec4414
lennartdeknikker <me@lenn4rt.com>
2019-06-11 17:48:21 +0200

    :sparkles: Add update method for my AI

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -11,30 +11,32 @@ const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${proce
 
 // create account
 exports.ai_newSync = function (req, res) {
-    res.render("pages/sync-module", {
+	res.render("pages/sync-module", {
 		title: "Sync new module",
-    });
+	});
 };
 
 // create
 exports.ai_create = function (req, res) {
-    mongoose.connect(url,  { useNewUrlParser: true }).then(
-        () => {
-            // construct model
+	mongoose.connect(url, {
+		useNewUrlParser: true
+	}).then(
+		() => {
+			// construct model
 			let AiSchema = mongoose.model("Ai", schema);
 
 			// format date so db is happy
 			let formatDate = moment(req.body.contructionDate).format("YYYY-DD-MM");
 
-            // create new instance of user
-            let ai = new AiSchema({
-                serialNr : req.body.serialNr,
-                password: req.body.password,
-                instanceName: req.body.instanceName,
-                brandName: req.body.brandName,
+			// create new instance of user
+			let ai = new AiSchema({
+				serialNr: req.body.serialNr,
+				password: req.body.password,
+				instanceName: req.body.instanceName,
+				brandName: req.body.brandName,
 				constructionDate: new Date(formatDate),
 				gender: req.body.gender,
-				properties : {
+				properties: {
 					intelligence: req.body.intelligence,
 					environment: req.body.environment,
 					shape: req.body.shape,
@@ -42,29 +44,50 @@ exports.ai_create = function (req, res) {
 				},
 			});
 
-            // save user
-            ai.save(function (err) {
+			// save user
+			ai.save(function (err) {
 				if (err) {
 					console.log(err);
-				}
-                else {
+				} else {
 					orbit.createOrbit(ai);
-                    res.send(`User created: \n ${ai}`);
-                }
+					res.send(`User created: \n ${ai}`);
+				}
 			});
 
 			//return res.redirect("myAI");
-        },
-        err => { console.log(err); }
-    );
+		},
+		err => {
+			console.log(err);
+		}
+	);
 };
 
 // update account
 exports.ai_update = function (req, res) {
-	utils.findAi(req.session.passport.user).then(function(ai) {
+	utils.findAi(req.session.passport.user).then(function (ai) {
 		res.render("pages/update", {
 			title: "Update my AI",
 			ai: ai
 		});
 	});
 };
+
+exports.ai_onUpdate = function (req, res) {
+		let AiSchema = mongoose.model("Ai", schema);
+		utils.findAi(req.session.passport.user).then(function (ai) {
+					AiSchema.findOneAndUpdate({
+						serialNr: ai.serialNr
+					}, {
+						gender: req.body.gender,
+						properties: {
+							intelligence: req.body.intelligence,
+							environment: req.body.environment,
+							shape: req.body.shape,
+							abilities: req.body.abilities
+						}
+					}).then(function () {
+							res.redirect("update");
+						}
+					);});
+
+				};
--- routes/ai.route.js
+++ routes/ai.route.js
@@ -5,5 +5,6 @@ const ai_controller = require("../controllers/ai.controller");
 router.get("/sync-module", ai_controller.ai_newSync);
 router.post("/create", ai_controller.ai_create);
 router.get("/update", ai_controller.ai_update);
+router.post("/update", ai_controller.ai_onUpdate);
 
 module.exports = router;
