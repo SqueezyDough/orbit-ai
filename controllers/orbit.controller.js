17adb1ad412905bdb3126cf56f0811ecdf165f2d
SqueezyDough <lvanbiljouw@hotmail.com>
2019-06-23 15:16:12 +0200

    :boom: Add connection overview
    - Add disconnect feature
    - The target orbit now only gets the caller's root orbit id, instead of the entire orbit, after connection
    - Only the target's root orbit from the caller's orbit gets removed after disconnect. The target orbit keeps the caller's root orbit id
    - Fix an issue where the ai's own orbit was counted towards the scout value
    - Fix an issue where it was possible to see your own planet in your orbit after a connection was made with an orbit containing your own orbit
    - Add extra comments to describe functionality

--- controllers/orbit.controller.js
+++ controllers/orbit.controller.js
@@ -1,31 +1,53 @@
 const mongoose = require("mongoose");
 const Ai = require("../models/ai.model");
+const AiSchema = require("../models/ai.model").AiSchema;
 const Orbit = require("../models/orbit.model").OrbitSchema;
 const utils = require("./utils/utils.controller");
+const _url = require("url");
 
 require("dotenv").config();
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
 
+mongoose.set("useFindAndModify", false);
+
 const orbitController = {};
 
+// explore someone elses orbit
 orbitController.explore = function(req, res) {
 	mongoose.connect(url, { useNewUrlParser: true });
 
 	utils.findOrbit(req.params.id).then(function(orbit) {
-		utils.findAi(orbit.ownerId).then(function(ai) {
-			res.render("pages/explore", {
-				title : `${process.env.APP_NAME} - ${ai.serialNr}'s Orbit`,
-				ai : ai,
-				url : "",
-				orbit : orbit,
-				abilities : ai.properties.abilities,
-				planets : orbit.planets,
-				isSynced: req.isAuthenticated()
+		// remove self from this orbit if found
+		utils.findOrbitByOwner(req.session.passport.user).then(function(ownOrbit) {
+			if (orbit.planets.includes(ownOrbit._id)) {
+				let index = orbit.planets.indexOf(ownOrbit._id);
+				orbit.planets.splice(index, 1);
+			}
+
+			utils.findAi(orbit.ownerId).then(function(ai) {
+				let connected = false;
+
+				// check if AI is connected
+				if (orbit.activeConnections.includes(req.session.passport.user) || req.query.connected) {
+					connected = true;
+				}
+
+				res.render("pages/explore", {
+					title : `${process.env.APP_NAME} - ${ai.serialNr}'s Orbit`,
+					ai : ai,
+					url : "",
+					orbit : orbit,
+					abilities : ai.properties.abilities,
+					planets : orbit.planets,
+					connected : connected,
+					isSynced: req.isAuthenticated()
+				});
 			});
 		});
 	});
 };
 
+// create a new orbit on account creation
 orbitController.createOrbit = async function(ai) {
 	mongoose.connect(url,  { useNewUrlParser: true });
 
@@ -39,26 +61,29 @@ orbitController.createOrbit = async function(ai) {
 
 	// find possible candidates
     Ai.find({ _id: { $ne: ai._id }}, (err, ais) => {
-		let matchedAis = 0;
+		// only look for a match if other ais are found
+		if (ais.length > 0) {
+			let matchedAis = 0;
 
-		// check for potential matches
-		ais.forEach( (_ai) => {
-			if (isMatch(ai, _ai)) {
-				matchedAis++;
-				// push matched ais to orbit
-				_ai.orbits.forEach( (_orbit) => {
-					orbit.planets.push(_orbit);
-				});
-			}
-		});
+			// check for potential matches
+			ais.forEach( (_ai) => {
+				if (isMatch(ai, _ai)) {
+					matchedAis++;
+					// push matched ais to orbit
+					_ai.orbits.forEach( (_orbit) => {
+						orbit.planets.push(_orbit);
+					});
+				}
+			});
 
-		// if no match is found just add the last 3.
-		if (matchedAis == 0) {
-			for (let i = (ais.length - 1); i >= (ais.length - 3); i--) {
-				try {
-					orbit.planets.push(ais[i].orbits[ais[i].orbits.length - 1]);
-				} catch (err) {
-					console.log(err);
+			// if no match is found just add the last 3.
+			if (matchedAis == 0) {
+				for (let i = (ais.length - 1); i >= (ais.length - 3); i--) {
+					try {
+						orbit.planets.push(ais[i].orbits[ais[i].orbits.length - 1]);
+					} catch (err) {
+						console.log(err);
+					}
 				}
 			}
 		}
@@ -80,55 +105,112 @@ orbitController.createOrbit = async function(ai) {
 	});
 };
 
+// connect 2 orbits and their owners
 orbitController.connectOrbits = function (req, res) {
-	utils.findAi(req.session.passport.user).then(function(callerAi) {
-		utils.findOrbitByOwner(callerAi._id).then(function(callerOrbit) {
+	try {
+		// find caller ai
+		utils.findAi(req.session.passport.user).then(function(callerAi) {
+			// find target orbit
 			utils.findOrbit(req.params.id).then(function(targetOrbit) {
-				utils.findAi(targetOrbit.ownerId).then(function(targetAi) {
-					targetOrbit.activeConnections.push(callerAi._id);
-					callerAi.orbits.push(targetOrbit._id);
-					targetAi.orbits.push(callerOrbit._id);
-					callerOrbit.activeConnections.push(targetAi._id);
+				// push target orbit to caller ai's orbit
+				callerAi.orbits.push(targetOrbit._id);
 
+				// save caller
+				callerAi.save(function (err) {
+					if (err) {
+						console.log(err);
+					}
+				});
+
+				// add the caller to the planets in the target orbit
+				// this will enable the owner from the target orbit to find the caller and move up in the orbit chain
+				utils.findOrbitByOwner(callerAi._id).then(function(callerOrbit) {
+					// check if logged in ai is not in this orbit
+					if (!targetOrbit.planets.includes(callerOrbit._id)) {
+						targetOrbit.planets.push(callerOrbit._id);
+					}
+
+					// add this connections to the target orbit
+					targetOrbit.activeConnections.push(callerAi._id);
+
+					// save target orbit
 					targetOrbit.save(function (err) {
 						if (err) {
 							console.log(err);
 						}
 					});
-
-					callerAi.save(function (err) {
-						if (err) {
-							console.log(err);
-						}
-					});
-
-					targetAi.save(function (err) {
-						if (err) {
-							console.log(err);
-						}
-					});
-
-					callerOrbit.save(function (err) {
-						if (err) {
-							console.log(err);
-						}
-					});
-
-					res.redirect(`./${req.params.id}`);
 				});
+
+				// redirect to orbit
+				res.redirect(_url.format({
+					pathname:`./${req.params.id}`,
+					query: {
+						"connected": true,
+					}
+				}));
 			});
+		});
+	} catch(err) {
+		console.log("connection failed", err);
+	}
+};
+
+// disconnect from someones orbit
+orbitController.disconnectFromOrbit = function(req, res) {
+	try {
+		// find caller ai
+		utils.findAi(req.session.passport.user).then(function(callerAi) {
+			// find target orbit
+			utils.findOrbit(req.params.id).then(function(targetOrbit) {
+				let OrbitSchema = mongoose.model("Orbit", Orbit);
+
+				OrbitSchema.findOneAndUpdate(
+					{ _id: targetOrbit._id },
+					{ "$pull": { "activeConnections": callerAi._id }},
+					{ safe: true, multi:true }, function(err) {
+						if (err) {
+							console.log(err);
+						}
+				});
+
+				let Schema = mongoose.model("Ai", AiSchema);
+
+				Schema.findOneAndUpdate(
+					{ _id: callerAi._id },
+					{ "$pull": { "orbits": targetOrbit._id }},
+					{ safe: true, multi:true }, function(err) {
+						if (err) {
+							console.log(err);
+						}
+				});
+
+				res.redirect("/ai/account");
+			});
+		});
+	} catch(err) {
+		console.log("disconnect failed", err);
+	}
+};
+
+// scout an orbit before exploring
+orbitController.scoutOrbit = function (req, res) {
+	utils.findOrbit(req.params.id).then(function(orbit) {
+		// count all planets in orbit
+		let satellitesInOrbit = orbit.planets.length;
+
+		utils.findOrbitByOwner(req.session.passport.user).then(function(ownerOrbit) {
+			// check if logged in ai's orbit is in orbit. If so, subtract in from counter
+			if (orbit.planets.includes(ownerOrbit._id)) {
+				satellitesInOrbit--;
+			}
+
+			res.write(JSON.stringify(satellitesInOrbit) );
+			res.end();
 		});
 	});
 };
 
-orbitController.scoutOrbit = function (req, res) {
-	utils.findOrbit(req.params.id).then(function(orbit) {
-		let satellitesInOrbit = orbit.planets.length;
-		res.write(JSON.stringify(satellitesInOrbit) );
-		res.end();
-	});
-};
-
+// check if two ai's match
 function isMatch(ai, candidateAi) {
 	let keys = Object.values(Object.keys(ai.properties));
 	let matchedProperties = 0;
